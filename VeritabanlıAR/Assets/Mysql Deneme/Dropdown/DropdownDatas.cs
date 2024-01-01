fff72935f137a5176b1bea08ac2b497b2427baea
Kaan Toraman <kaan03tor@gmail.com>
2024-01-01 20:28:35 +0300

    Dropdown Data Handlers

---
+++ VeritabanlıAR/Assets/Mysql Deneme/Dropdown/DropdownDatas.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropdownDatas : MonoBehaviour
+{
+    [Header("Ogretmenler")]
+    [SerializeField] TMP_Dropdown ogretmenlerDropDown;
+    List<string> ogretmenlerID=new List<string>();
+    List<string> ogretmenlerAd=new List<string>();
+    string[] ogretmenler;
+
+    /* Bunlar önemli*/
+    public string seciliOgretmenID; 
+    public string seciliOgretmenAD;
+
+    [Header("Dersler")]
+    [SerializeField] TMP_Dropdown derslerDropDown;
+    string[] dersler;
+    List<string> dersID = new List<string>();
+    List<string> dersADI = new List<string>();
+    List<string> dersSinifi = new List<string>();
+
+
+    /* Bunlar önemli*/
+    public string seciliDersID;
+    public string seciliDersADI;
+    public string seciliDersSinifi;
+
+    MysqlDataFetch dataFetch;
+
+    #region Ogretmenler
+    void LoadOgretmenler()
+    {
+        for (int i = 0; i < ogretmenler.Length - 1; i++)
+        {
+            string[] parcalar = ogretmenler[i].Trim().Split(",");
+            ogretmenlerID.Add(parcalar[0]);
+            ogretmenlerAd.Add(parcalar[1]); 
+
+            var option=new TMP_Dropdown.OptionData(parcalar[1]);
+            ogretmenlerDropDown.options.Add(option);
+        }
+        
+    }
+    public void HandleOgretmenlerData()
+    {
+        string valString = ogretmenlerDropDown.value.ToString();
+        int size = ogretmenlerAd.Count;
+        for(int i = 0; i < size; i++)
+        {
+            if (valString.Equals(ogretmenlerID[i]))
+            {
+                seciliOgretmenID = ogretmenlerID[i];
+                seciliOgretmenAD = ogretmenlerAd[i];
+            }
+        }
+    }
+    #endregion Ogretmenler
+
+    #region Dersler
+    void LoadDersler()
+    {
+        for(int i=0; i<dersler.Length-1; i++)
+        {
+            string[] parcalar = dersler[i].Trim().Split(",");
+            dersID.Add(parcalar[0]);
+            dersADI.Add(parcalar[1]);
+            dersSinifi.Add(parcalar[2]);
+
+            var option = new TMP_Dropdown.OptionData(parcalar[1] + " / " + parcalar[2]+". sýnýf");
+            derslerDropDown.options.Add(option);
+        }
+
+    }
+
+    public void HandleDerslerData()
+    {
+        string valString = derslerDropDown.value.ToString();
+        int size = dersADI.Count;
+        for (int i = 0; i < size; i++)
+        {
+            if (valString.Equals(dersID[i]))
+            {
+                seciliDersID = dersID[i];
+                seciliDersADI = dersADI[i];
+                seciliDersSinifi = dersSinifi[i];
+            }
+        }
+    }
+    #endregion Dersler
+    private void Start()
+    {
+        dataFetch=FindObjectOfType<MysqlDataFetch>(); 
+        ogretmenler=dataFetch.OgretmenlerMetot();
+        dersler=dataFetch.DerslerMetot();
+        LoadOgretmenler();
+        LoadDersler();
+    }
+    private void Update()
+    {
+    }
+}
--- VeritabanlıAR/Assets/Mysql Deneme/MysqlScripts/MysqlDataFetch.cs
+++ VeritabanlıAR/Assets/Mysql Deneme/MysqlScripts/MysqlDataFetch.cs
@@ -12,13 +12,20 @@ public class MysqlDataFetch : MonoBehaviour
     public string[] dersOgretmen;
     public string[] ogretmenler;
 
-    void Start()
+    void Awake()
     {
         StartCoroutine(getDersProgrami());
         StartCoroutine(getDersler());
         StartCoroutine(getDersOgretmen());
         StartCoroutine(getOgretmen());
     }
+    //private void Update()
+    //{
+    //    StartCoroutine(getDersProgrami());
+    //    StartCoroutine(getDersler());
+    //    StartCoroutine(getDersOgretmen());
+    //    StartCoroutine(getOgretmen());
+    //}
 
     public IEnumerator getDersProgrami()
     {
@@ -37,24 +44,8 @@ public class MysqlDataFetch : MonoBehaviour
             //dördüncü gun_id
             //besinci saat_id
             //altinci ders_sinif    
-            string[] cogul = s.Split("<br>");
-            int size= cogul.Length;
-            dersProgramlari= new string[size];
-            int i = 0;
-
-            foreach (string he in cogul)
-            {
-                if (i < dersProgramlari.Length)
-                {
-                    dersProgramlari[i] +=he;
-                    i++;
-                }
-                else
-                {
-                    break;
-                }
-
-            }
+            dersProgramlari = s.Split("<br>");
+            
             foreach (string za in dersProgramlari)
             {
                 za.Trim();
@@ -77,24 +68,8 @@ public class MysqlDataFetch : MonoBehaviour
             //ikinci ders_adi
             //ücüncü ders_sinif
              
-            string[] cogul = s.Split("<br>");
-            int size = cogul.Length;
-            dersler = new string[size];
-            int i = 0;
-
-            foreach (string he in cogul)
-            {
-                if (i < dersler.Length)
-                {
-                    dersler[i] += he;
-                    i++;
-                }
-                else
-                {
-                    break;
-                }
-
-            }
+            dersler = s.Split("<br>");
+            
             foreach (string za in dersler)
             {
                 za.Trim();
@@ -117,24 +92,9 @@ public class MysqlDataFetch : MonoBehaviour
             //ikinci ders_id
             //ücüncü ogretmen_id
 
-            string[] cogul = s.Split("<br>");
-            int size = cogul.Length;
-            dersOgretmen = new string[size];
-            int i = 0;
-
-            foreach (string he in cogul)
-            {
-                if (i < dersOgretmen.Length)
-                {
-                    dersOgretmen[i] += he;
-                    i++;
-                }
-                else
-                {
-                    break;
-                }
-
-            }
+             dersOgretmen = s.Split("<br>");
+            
+            
             foreach (string za in dersOgretmen)
             {
                 za.Trim();
@@ -144,7 +104,7 @@ public class MysqlDataFetch : MonoBehaviour
 
     public IEnumerator getOgretmen()
     {
-        UnityWebRequest www = UnityWebRequest.Get(server_url + "/dersOgretmen.php");
+        UnityWebRequest www = UnityWebRequest.Get(server_url + "/ogretmenler.php");
         yield return www.SendWebRequest();
         if (www.isHttpError || www.isNetworkError)
         {
@@ -156,29 +116,23 @@ public class MysqlDataFetch : MonoBehaviour
             //ilk ogretmen_id
             //ikinci ad
 
-            string[] cogul = s.Split("<br>");
-            int size = cogul.Length;
-            ogretmenler = new string[size];
-            int i = 0;
-
-            foreach (string he in cogul)
-            {
-                if (i < ogretmenler.Length)
-                {
-                    ogretmenler[i] += he;
-                    i++;
-                }
-                else
-                {
-                    break;
-                }
-
-            }
+            ogretmenler = s.Split("<br>");
+            
             foreach (string za in ogretmenler)
             {
                 za.Trim();
             }
         }
+    }
+
+    public string[] OgretmenlerMetot()
+    {
+        return ogretmenler;
+    }
+
+    public string[] DerslerMetot()
+    {
+        return dersler;
     }
 
 }
